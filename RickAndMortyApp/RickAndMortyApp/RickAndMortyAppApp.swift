c73cbd8410e864d5c1650d833daa489157b8944f
aorti <aorti>
2024-05-17 09:08:00 +0200

    feat: RickAndMortyApp project added

---
+++ RickAndMortyApp/RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Alvaro Orti Moreno on 17/5/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
